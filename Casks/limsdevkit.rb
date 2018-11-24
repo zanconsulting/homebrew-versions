6e1e1bfdbebf1b6748e02015d296975bdf642e09
Alex Dean <alex@zanconsulting.com>
2018-11-24 12:31:55 -0700

    limsdevkit - add postflight and bump to 0.0.3

--- Casks/limsdevkit.rb
+++ Casks/limsdevkit.rb
@@ -1,12 +1,27 @@
 cask 'limsdevkit' do
-  version '0.0.1'
-  sha256 '5e70de42043947f6ba396e1c1bda05dfdbaa84634ecfe4ea9357c2efaf38ce49'
+  version '0.0.3'
+  sha256 'efba19818a754a81de174ffd5684a113ab733f7c2a24c364c6518284c4c9ee31'
 
-  url "https://github.com/zanconsulting/native-messaging-tester/releases/download/#{version}/zan-native-messaging-tester-#{version}-osx-x64.dmg"
   url "https://f001.backblazeb2.com/file/zan-public/homebrew/limsdevkit-v#{version}.dmg"
-  
+
   name 'LIMS Devkit'
-  #hompage ""
+  #homepage ""
 
   app 'LIMSDevkit.app'
+
+  postflight do
+    # Run application once to register protocol handlers
+    system_command "#{appdir}/LIMSDevkit.app/Contents/MacOS/limsdevkit",
+             args: ['--setup-and-quit'],
+             sudo: false,
+             print_stderr: false
+
+    # Symlink CLI to /usr/local/bin
+    system_command "ln",
+             args: ['-sf', "#{appdir}/LIMSDevkit.app/Contents/Resources/app/bin/limsdevkit", "/usr/local/bin/limsdevkit"]
+
+  end
+
+  caveats "The limsdevkit CLI application is included in this package and will be linked in /usr/local/bin/"
+
 end
